9a15aaf8769cdf1e59978ab320c8c86d9a8614b5
michaelc <michael.38@outlook.com>
2018-03-01 14:21:40 -0500

    applied basic authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+    http_basic_authenticate_with :name => ENV['ADMIN_SECRET_USERNAME'], :password => ENV['ADMIN_SECRET_PASSWORD']
+
     def index
         @category = Category.all
     end
