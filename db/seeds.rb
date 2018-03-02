5af286815846927ba56ab023116b16292160dcb7
michaelc <michael.38@outlook.com>
2018-03-02 18:13:29 -0500

    revised seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,148 +26,171 @@ end
 puts "Finding or Creating Categories ..."
 
 cat1 = Category.find_or_create_by! name: 'Apparel'
-cat2 = Category.find_or_create_by! name: 'Electronics'
-cat3 = Category.find_or_create_by! name: 'Furniture'
+cat2 = Category.find_or_create_by! name: 'Footwear'
+cat3 = Category.find_or_create_by! name: 'Accessories'
 
 ## PRODUCTS
 
-puts "Re-creating Products ..."
+puts "Re-creating Seed Products ..."
 
 Product.destroy_all
 
 cat1.products.create!({
-  name:  'Men\'s Classy shirt',
-  description: Faker::Hipster.paragraph(4),
+  name:  'COMMES DES GARCONS HOODIE',
+  image: open_asset('apparel0.jpg'),
+  quantity: 6,
+  price: 126.00
+})
+
+
+cat1.products.create!({
+  name:  'COMMES DES GARCONS HOODIE',
   image: open_asset('apparel1.jpg'),
-  quantity: 10,
-  price: 64.99
-})
-
-cat1.products.create!({
-  name:  'Women\'s Zebra pants',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel2.jpg'),
-  quantity: 18,
-  price: 124.99
-})
-
-cat1.products.create!({
-  name:  'Hipster Hat',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel3.jpg'),
-  quantity: 4,
-  price: 34.49
-})
-
-cat1.products.create!({
-  name:  'Hipster Socks',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel4.jpg'),
-  quantity: 8,
-  price: 25.00
-})
-
-cat1.products.create!({
-  name:  'Russian Spy Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel5.jpg'),
-  quantity: 8,
-  price: 1_225.00
-})
-
-cat1.products.create!({
-  name:  'Human Feet Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel6.jpg'),
-  quantity: 82,
-  price: 224.50
-})
-
-
-cat2.products.create!({
-  name:  'Modern Skateboards',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics1.jpg'),
-  quantity: 40,
-  price: 164.49
-})
-
-cat2.products.create!({
-  name:  'Hotdog Slicer',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics2.jpg'),
   quantity: 3,
-  price: 26.00
+  price: 126.00
+})
+
+
+cat1.products.create!({
+  name:  'ADIDAS X NEIGHBORHOOD ',
+  image: open_asset('apparel3.jpg'),
+  quantity: 23,
+  price: 95.00
+})
+
+cat1.products.create!({
+  name:  'STONE ISLAND HOODIE ',
+  image: open_asset('apparel4.jpg'),
+  quantity: 31,
+  price: 136.00
+})
+
+
+cat1.products.create!({
+  name:  'STONE ISLAND HOODIE ',
+  image: open_asset('apparel5.jpg'),
+  quantity: 30,
+  price: 136.00
+})
+
+
+cat2.products.create!({
+  name:  'ADIDAS NMD R2 / CORE BLACK',
+  image: open_asset('footwear1.jpg'),
+  quantity: 6,
+  price: 265.00
 })
 
 cat2.products.create!({
-  name:  'World\'s Largest Smartwatch',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics3.jpg'),
-  quantity: 32,
-  price: 2_026.29
+  name:  'ADIDAS NMD R2 / WHITE',
+  image: open_asset('footwear2.jpg'),
+  quantity: 8,
+  price: 265.00
 })
 
-cat3.products.create!({
-  name:  'Optimal Sleeping Bed',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture1.jpg'),
-  quantity: 0,
-  price: 3_052.00
+
+cat2.products.create!({
+  name:  'Y-3 SAIKOU / CORE WHITE',
+  image: open_asset('footwear4.jpg'),
+  quantity: 22,
+  price: 225.00
 })
 
-cat3.products.create!({
-  name:  'Electric Chair',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture2.jpg'),
+cat2.products.create!({
+  name:  'Y-3 SAIKOU / CORE BLACK',
+  image: open_asset('footwear5.jpg'),
+  quantity: 16,
+  price: 225.00
+})
+
+cat2.products.create!({
+  name:  'NIKE AIR MAX 270 QS / HOT PUNCH',
+  image: open_asset('footwear6.jpg'),
   quantity: 2,
-  price: 987.65
+  price: 204.50
 })
+
+
+cat2.products.create!({
+  name:  'NIKE AIR MAX 270 QS / TOTAL ORANGE',
+  image: open_asset('footwear7.jpg'),
+  quantity: 4,
+  price: 204.50
+})
+
+cat2.products.create!({
+  name:  'NIKE AIR JORDAN 1',
+  image: open_asset('footwear10.jpg'),
+  quantity: 10,
+  price: 169.50
+})
+
+
+cat2.products.create!({
+  name:  'NIKE AIR JORDAN 1',
+  image: open_asset('footwear11.jpg'),
+  quantity: 20,
+  price: 169.50
+})
+
+
+
+
 
 cat3.products.create!({
-  name:  'Red Bookshelf',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture3.jpg'),
-  quantity: 0,
-  price: 2_483.75
+  name:  'NEIGHBORHOOD SRL /EC-CAP',
+  image: open_asset('accessory1.jpg'),
+  quantity: 12,
+  price: 24.50
 })
 
-puts "Re-creating Users ..."
+
+cat3.products.create!({
+  name:  'NEIGHBORHOOD SRL /EC-CAP',
+  image: open_asset('accessory2.jpg'),
+  quantity: 8,
+  price: 24.50
+})
+
+
+
+
+puts "Re-creating Seed Users ..."
 
 User.create!({
-  name: 'Michael',
+  name: 'michael38',
   email: 'aaa@email.com',
   password: 'abc'
 })
 
 User.create!({
-  name: 'Joanne',
+  name: 'joanne_12',
   email: 'bbb@email.com',
   password: 'abc'
 })
 
- puts "Re-creating Reviews ..."
+ puts "Re-creating Seed Reviews ..."
 
 Review.destroy_all
 
 Review.create!({
-  product_id: 1,
+  product_id: 6,
   user_id: 1,
-  description: 'This sucks',
-  rating: 2
+  description: 'Really comfortable! Love these shoes!',
+  rating: 4
 })
 
 Review.create!({
-  product_id: 1,
+  product_id: 6,
   user_id: 2,
-  description: 'This is weird',
-  rating: 3
+  description: 'I love this so much <3',
+  rating: 5
 })
 
 Review.create!({
-  product_id: 2,
+  product_id: 8,
   user_id: 1,
-  description: 'amazing',
+  description: 'Amazing!!',
   rating: 5
 })
 
