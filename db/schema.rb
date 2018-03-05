cee557695685708830f8b4c98e5b157529b635d5
michael-38 <michael.38@outlook.com>
2018-03-05 15:50:59 +0000

    prepare for testing

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,4 @@ ActiveRecord::Schema.define(version: 20180301201647) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
-  # add_foreign_key "reviews", "products"
-  # add_foreign_key "reviews", "users"
 end
